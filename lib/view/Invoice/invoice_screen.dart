da2b786adfbd9ec1ce295279207493b5c49329a9
Rafayxee <rafaymashoor@hotmail.com>
2025-04-16 02:02:14 +0500

    added bill and shipping container

--- lib/view/Invoice/invoice_screen.dart
+++ lib/view/Invoice/invoice_screen.dart
@@ -26,12 +26,21 @@ class InvoiceScreen extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                Text(
-                  'Created on ${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}',
-                  style: GoogleFonts.poppins(
-                    fontSize: 12,
-                    fontWeight: FontWeight.normal,
-                  ),
+                Row(
+                  children: [
+                    Text(
+                      'Created on ${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}',
+                      style: GoogleFonts.poppins(
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    const Spacer(),
+                    const Icon(
+                      Icons.navigate_next_sharp,
+                      size: 30,
+                    ),
+                  ],
                 ),
                 Text(
                   'Due on ${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}',
@@ -43,39 +52,79 @@ class InvoiceScreen extends StatelessWidget {
               ],
             ),
           ),
-          Expanded(
-            child: Container(
-              margin: const EdgeInsets.all(12),
-              padding: const EdgeInsets.all(12),
-              width: MediaQuery.sizeOf(context).width,
-              decoration: BoxDecoration(
-                  color: Colors.grey, borderRadius: BorderRadius.circular(9)),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Invoice',
-                    style: GoogleFonts.poppins(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+          Container(
+            margin: const EdgeInsets.all(12),
+            padding: const EdgeInsets.all(12),
+            width: MediaQuery.sizeOf(context).width,
+            decoration: BoxDecoration(
+                color: Colors.grey, borderRadius: BorderRadius.circular(9)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'Bill From',
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  Text(
-                    'Created on ${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}',
-                    style: GoogleFonts.poppins(
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal,
+                    const Spacer(),
+                    const Icon(
+                      Icons.navigate_next_sharp,
+                      size: 30,
                     ),
-                  ),
-                  Text(
-                    'Due on ${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}',
-                    style: GoogleFonts.poppins(
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal,
+                  ],
+                ),
+                const SizedBox(height: 10),
+                const Divider(),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text(
+                      'Bill To',
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                ],
-              ),
+                    const Spacer(),
+                    const Icon(
+                      Icons.navigate_next_sharp,
+                      size: 30,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(12),
+            padding: const EdgeInsets.all(12),
+            width: MediaQuery.sizeOf(context).width,
+            decoration: BoxDecoration(
+                color: Colors.grey, borderRadius: BorderRadius.circular(9)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'Shipping Details: ',
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Spacer(),
+                    const Icon(
+                      Icons.navigate_next_sharp,
+                      size: 30,
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ],
